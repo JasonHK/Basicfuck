eec761521d162a9f83adc82deb85344409525999
Kwok Chi Leong <JasonHK@users.noreply.github.com>
2016-12-05 17:40:16 +0800

    Fixed a serious bug
    Thanks @rdebath

--- Brainfuck.vb
+++ Brainfuck.vb
@@ -19,14 +19,15 @@
         Dim CellPointer As UInteger = 0
         Dim LoopDepth As Integer = 0
         Dim LoopJump(1000) As Integer
-        Dim InitialComment As Boolean = False
+        Dim CommentLoop As Boolean = False
+        Dim CommentDepth As Integer = 0
         Dim SkipDelay As Boolean = False
 
         For index = 0 To CodeLength - 1
             If Not ForceStop Then
                 Select Case Code.Chars(index)
                     Case ">"
-                        If Not InitialComment Then
+                        If Not CommentLoop Then
                             Try
                                 CellPointer += 1
                             Catch ex As OverflowException
@@ -34,7 +35,7 @@
                             End Try
                         End If
                     Case "<"
-                        If Not InitialComment Then
+                        If Not CommentLoop Then
                             Try
                                 CellPointer -= 1
                             Catch ex As OverflowException
@@ -42,7 +43,7 @@
                             End Try
                         End If
                     Case "+"
-                        If Not InitialComment Then
+                        If Not CommentLoop Then
                             Try
                                 Cell(CellPointer) += 1
                             Catch ex As OverflowException
@@ -50,7 +51,7 @@
                             End Try
                         End If
                     Case "-"
-                        If Not InitialComment Then
+                        If Not CommentLoop Then
                             Try
                                 Cell(CellPointer) -= 1
                             Catch ex As OverflowException
@@ -58,25 +59,33 @@
                             End Try
                         End If
                     Case "."
-                        If Not InitialComment Then MainForm.BrainfuckOutput(Cell(CellPointer))
+                        If Not CommentLoop Then MainForm.BrainfuckOutput(Cell(CellPointer))
                     Case ","
-                        If Not InitialComment Then Cell(CellPointer) = MainForm.BrainfuckInput()
+                        If Not CommentLoop Then Cell(CellPointer) = MainForm.BrainfuckInput()
                     Case "["
-                        If index = 0 Then InitialComment = True
-                        LoopDepth += 1
-                        LoopJump(LoopDepth) = index
-                    Case "]"
-                        If Cell(CellPointer) = 0 Then
-                            LoopDepth -= 1
+                        If Cell(CellPointer) = 0 And Not CommentLoop Then
+                            CommentLoop = True
+                            CommentDepth += 1
+                        ElseIf CommentLoop Then
+                            CommentDepth += 1
                         Else
-                            index = LoopJump(LoopDepth)
+                            LoopDepth += 1
+                            LoopJump(LoopDepth) = index
                         End If
-                        If LoopDepth = 0 Then InitialComment = False
+                    Case "]"
+                        If Cell(CellPointer) = 0 And Not CommentLoop Then
+                            LoopDepth -= 1
+                        ElseIf Not CommentLoop Then
+                            index = LoopJump(LoopDepth)
+                        Else
+                            CommentDepth -= 1
+                            If CommentDepth = 0 Then CommentLoop = False
+                        End If
                     Case Else
                         SkipDelay = True
                 End Select
 
-                If Not SkipDelay And Not InitialComment Then
+                If Not SkipDelay And Not CommentLoop Then
                     MainForm.BrainfuckPointer(CellPointer, Cell(CellPointer))
                     Debug.WriteLine("Cell: " & CStr(CellPointer))
                     Debug.WriteLine("Value: " & CStr(Cell(CellPointer)))
